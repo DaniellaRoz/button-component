e2ebbb8d30624086dfb844ef670647c4a59d02fd
DaniellaRoz <114352757+DaniellaRoz@users.noreply.github.com>
2023-05-11 14:02:46 +0200

    finish navbar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,7 +1,18 @@
+import "./NavBar.css";
+
 function NavBar( {id} ) {
     return (
         <div id={id}>
-            placeholder for navbar
+            <p id="logo"><span>Dev</span>challenges.io</p>
+
+            <ul>
+                <li>Colors</li>
+                <li>Typography</li>
+                <li>Spaces</li>
+                <li>Buttons</li>
+                <li><strong>Inputs</strong></li>
+                <li>Grid</li>
+            </ul>
         </div>
     );
 }
